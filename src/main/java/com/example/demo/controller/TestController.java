16890767dcb38d6c7f242d156a34659956dc5297
nam-yeun-hwa <nyh2ya@gamil.com>
2023-09-07 19:42:07 +0900

    Feat(TestController, TodoController, TodoService, TodoRequestBodyDTO):생성 및 수정작업

--- src/main/java/com/example/demo/controller/TestController.java
+++ src/main/java/com/example/demo/controller/TestController.java
@@ -1,17 +1,51 @@
 package com.example.demo.controller;
 
+import com.example.demo.dto.ResponseDTO;
+import com.example.demo.dto.TestRequestBodyDTO;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController
 @RequestMapping("test")
 public class TestController {
     @GetMapping
+    //문자열 반환
     public String testController(){
         return "hello World!";
     }
+
+    @GetMapping("/testRequestBody")
+    //DTO 반환
+    public String testControllerRequestBody(@RequestBody TestRequestBodyDTO testRequestBodyDTO){
+        return "Hello World ID" + testRequestBodyDTO.getId() + "Message : " + testRequestBodyDTO.getMessage();
+    }
+
+    @GetMapping("/testResponseBody")
+    //ResponseDTO 반환
+    public ResponseDTO<String> testControllerResponseBody(){
+        List<String> list = new ArrayList<>();
+        list.add("Hello World! I'm ResponseDTO");
+
+        ResponseDTO<String> response = ResponseDTO.<String>builder().data(list).build();
+        return response;
+    }
+
+    @GetMapping("/testResponseEntity")
+    //ResponseEntity 반환
+    public ResponseEntity<?> testControllerResponseEnity(){
+        List<String> list = new ArrayList<>();
+        list.add("Hello World! I'm ResponseEntity. And you got 400");
+        ResponseDTO<String> response = ResponseDTO.<String>builder().data(list).build();
+        return ResponseEntity.badRequest().body(response);
+
+    }
 }
 
-//@RestController를 이용하면 hppt 관련된 코드 및 요청/응답 매핑을 스프링이 알아서 해준다.
+//@RestController를 이용하면 http 관련된 코드 및 요청/응답 매핑을 스프링이 알아서 해준다.
 //localhost:8080/test의 GET 호출에는 testController()가 호출 된다.
