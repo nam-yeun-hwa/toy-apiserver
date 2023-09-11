3be8193aaabd7513088b7373ebc2e652e9a3518b
nam-yeun-hwa <nyh2ya@gamil.com>
2023-09-12 00:37:03 +0900

    Feat(TodoService):create, validate 추가

--- src/main/java/com/example/demo/service/TodoService.java
+++ src/main/java/com/example/demo/service/TodoService.java
@@ -2,14 +2,42 @@ package com.example.demo.service;
 
 import com.example.demo.model.TodoEntity;
 import com.example.demo.persistence.TodoRepository;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
+@Slf4j
 @Service
 public class TodoService{
 
     @Autowired
     private TodoRepository repository;
+
+    public List<TodoEntity> create(final TodoEntity entity){
+        //validations
+//        validate(entity);
+
+        repository.save(entity);
+        log.info("Entity Id : {} is saved.", entity.getId());
+
+        return  repository.findByUserId(entity.getUserId());
+//        return repository.findbyUserId(entity.getUserId());
+    }
+
+    private void validate(final TodoEntity entity){
+        if(entity == null){
+            log.warn("Entity cannot be null.");
+
+            throw new RuntimeException("Entity cannot be null");
+        }
+
+        if(entity.getUserId() == null){
+            log.warn("Unknown user");
+            throw new RuntimeException("Unknow user.");
+        }
+    }
 
     public String testService(){
 
