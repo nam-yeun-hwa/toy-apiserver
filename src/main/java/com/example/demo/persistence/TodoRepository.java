45317297e2f3af76866e9267ea13add29c637048
nam-yeun-hwa <nyh2ya@gamil.com>
2023-09-12 00:32:51 +0900

    Feat(TodoRepository):findByUserId 추가

--- src/main/java/com/example/demo/persistence/TodoRepository.java
+++ src/main/java/com/example/demo/persistence/TodoRepository.java
@@ -4,8 +4,11 @@ import com.example.demo.model.TodoEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface TodoRepository extends JpaRepository<TodoEntity, String> {
+    List<TodoEntity> findByUserId(String userId);
 
 }
 
