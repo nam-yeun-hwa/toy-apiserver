5c7333783be2e03ddf672bd717a233d2295dbbc9
nam-yeun-hwa <nyh2ya@gamil.com>
2023-09-15 21:15:18 +0900

    Feat(TodoService, TodoController): 유저 아이디를 이용한 게시글 검색

--- src/main/java/com/example/demo/controller/TodoController.java
+++ src/main/java/com/example/demo/controller/TodoController.java
@@ -4,6 +4,7 @@ import com.example.demo.dto.ResponseDTO;
 import com.example.demo.dto.TodoDTO;
 import com.example.demo.model.TodoEntity;
 import com.example.demo.service.TodoService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -12,6 +13,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 
+@Slf4j
 @RestController
 @RequestMapping("todo")
 public class TodoController {
@@ -24,6 +26,7 @@ public class TodoController {
             String temporaryUserId = "temporary-user";
             //dto를 엔티티로 변환
             TodoEntity entity = TodoDTO.todoEntity(dto);
+
             //id를 null로 초기화한다.
             //생성 당시에는 id가 없어야 하기 때문이다.
             entity.setId(null);
@@ -33,6 +36,7 @@ public class TodoController {
 
             //서비스를 이용한 Todo엔티티를 생성한다.
             List<TodoEntity> entities = service.create(entity);
+//            log.info("entity : {} 전달", entities);
 
             //자바스트림을 이용해 리턴된 엔티티 리스트를 TodoDTO리스트로 변환한다.
             List<TodoDTO> dtos = entities.stream().map(TodoDTO::new).collect(Collectors.toList());
@@ -48,6 +52,19 @@ public class TodoController {
             ResponseDTO<TodoDTO> response = ResponseDTO.<TodoDTO>builder().error(error).build();
             return  ResponseEntity.badRequest().body(response);
         }
+    }
+
+    @GetMapping
+    public ResponseEntity<?> retrieveTodoList(){
+        String temporaryUserId = "temporary-user";
+
+        List<TodoEntity> entities = service.retrieve(temporaryUserId);
+
+        List<TodoDTO> dtos = entities.stream().map(TodoDTO::new).collect(Collectors.toList());
+
+        ResponseDTO<TodoDTO> response = ResponseDTO.<TodoDTO>builder().data(dtos).build();
+
+        return  ResponseEntity.ok().body(response);
     }
 
     @GetMapping("/test")
