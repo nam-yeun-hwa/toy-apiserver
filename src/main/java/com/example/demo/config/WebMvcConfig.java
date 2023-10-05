bbcffe4f6d4e741f64fa3d2237e160ddd2b12ead
nam-yeun-hwa <nyh2ya@gmail.com>
2023-10-06 01:13:23 +0900

    test(WebMvcConfig): CORS 관련 allowedMethods 전체 접근으로 변경

--- src/main/java/com/example/demo/config/WebMvcConfig.java
+++ src/main/java/com/example/demo/config/WebMvcConfig.java
@@ -13,8 +13,8 @@ public class WebMvcConfig implements WebMvcConfigurer {
         registry.addMapping("/**")
 
                 .allowedOrigins("http://localhost:3000")
-
-                .allowedMethods("GET","POST","PUT","PATCH","DELETE","OPTIONS")
+                .allowedMethods("*")
+//                .allowedMethods("GET","POST","PUT","PATCH","DELETE","OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true)
                 .maxAge(MAX_AGE_SECS);
